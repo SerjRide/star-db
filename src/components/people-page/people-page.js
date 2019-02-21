51bfe0102a21808ac11445fae748c58f2f8ff31f
SerjRide <quicksquicks6@gmail.com>
2019-02-21 10:49:49 +0300

    add container property

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 import ItemList from '../item-list/item-list';
 import PersonDetails from '../person-details/person-details';
 import ErrorIndicator from '../error-indicator/error-indicator';
+import Row from '../row'
 import SwapiService from '../../services/swapi-service';
-
 import './people-page.css';
 
 export default class PeoplePage extends Component {
@@ -33,17 +33,19 @@ export default class PeoplePage extends Component {
       return <ErrorIndicator />;
     }
 
+    const itemList = (
+      <ItemList onItemSelected = {this.onPersonSelected}
+        getData = {this.swapiService.getAllPeople}
+        renderItem = {({name, gender, birthYear}) => (
+          `${name} (${gender}, ${birthYear})`)}/>
+    );
+
+    const personDetails = (
+      <PersonDetails personId={this.state.selectedPerson} />
+    );
+
     return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList onItemSelected={this.onPersonSelected}
-          getData = {this.swapiService.getAllPeople}
-          renderItem={({name, gender, birthYear}) => `${name} (${gender}, ${birthYear})`}/>
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
-      </div>
+      <Row left={itemList} right={personDetails}/>
     );
   }
 }
